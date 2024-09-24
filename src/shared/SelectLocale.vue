0c3b8fcf5b8f6c3a1e2a703064316ade188c5339
steve.kim <steve.kim@aimatics.ai>
2024-09-24 18:06:52 +0900

    Init
    Init commit

--- src/shared/SelectLocale.vue
+++ src/shared/SelectLocale.vue
@@ -1,11 +1,28 @@
 <script setup lang="ts">
+interface SelectLocaleEmits {
+  (e: 'onChangeLang', lang: string): void;
+}
 
+const langs = ['ko', 'en', 'ja'];
+const emits = defineEmits<SelectLocaleEmits>();
+
+const onChangeLang = (lang: string) => {
+  emits('onChangeLang', lang);
+};
 </script>
 
 <template>
-  $END$
+  <div class="lang_btn" v-for="lang in langs">
+    <button @click="onChangeLang(lang)">{{ lang }}</button>
+  </div>
 </template>
 
 <style scoped>
+@tailwind base;
+@tailwind components;
+@tailwind utilities;
 
+.lang_btn {
+  @apply z-[3] relative bg-white w-[5rem];
+}
 </style>
--- src/widgets/GoogleMaps.tsx
+++ src/widgets/GoogleMaps.tsx
@@ -1,63 +1,70 @@
----
-import { GOOGLE_MAP_KEY } from 'astro:env/client';
-import React, { useState, useCallback, useRef } from 'react';
+import React, { useState, useCallback, memo } from 'react';
 import { GoogleMap, Marker, useJsApiLoader } from '@react-google-maps/api';
 
 const containerStyle = {
   width: '100%',
   height: '100vh',
-  zIndex: 1,
 };
 
 const center = {
-  lat: -3.745,
-  lng: -38.523,
+  lat: 0,
+  lng: 0,
 };
 
-interface GoogleMapsProps {
-  mapKey: string;
-  children?: React.ReactNode;
+const worldBounds = {
+  north: 85, // 북쪽 경계 (북극 근처)
+  south: -85, // 남쪽 경계 (남극 근처)
+  east: 180, // 동쪽 경계 (180도)
+  west: -180, // 서쪽 경계 (-180도)
+};
+
+function GoogleMaps({ mapKey, children }: GoogleMapsProps) {
+  const { isLoaded } = useJsApiLoader({
+    id: 'google-map-script',
+    googleMapsApiKey: mapKey,
+    language: 'ko',
+    libraries: ['geocoding', 'geometry', 'visualization', 'drawing', 'places'],
+  });
+
+  const [map, setMap] = useState(null);
+
+  const onLoad = useCallback(function callback(map: any) {
+    const bounds = new window.google.maps.LatLngBounds(center);
+    map.fitBounds(bounds);
+
+    setMap(map);
+  }, []);
+
+  const onUnmount = useCallback(function callback() {
+    setMap(null);
+  }, []);
+
+  if (!isLoaded) {
+    return <></>;
+  }
+
+  return (
+    <GoogleMap
+      mapContainerStyle={containerStyle}
+      center={center}
+      zoom={3}
+      onLoad={onLoad}
+      onUnmount={onUnmount}
+      options={{
+        minZoom: 3,
+        streetView: null,
+        disableDefaultUI: true,
+        restriction: {
+          latLngBounds: worldBounds, // 지정된 영역
+          strictBounds: false, // 엄격한 제한 적용
+        },
+      }}
+    >
+      {children}
+
+      <Marker position={center} />
+    </GoogleMap>
+  );
 }
 
-const { isLoaded } = useJsApiLoader({
-  id: 'google-map-script',
-  googleMapsApiKey: GOOGLE_MAP_KEY,
-  language: 'ko',
-  libraries: ['geocoding', 'geometry'],
-});
-
-const [map, setMap] = useState(null);
-
-const onLoad = useCallback(function callback(map: any) {
-  const bounds = new window.google.maps.LatLngBounds(center);
-  map.fitBounds(bounds);
-
-  setMap(map);
-}, []);
-
-const onUnmount = useCallback(function callback() {
-  setMap(null);
-}, []);
----
-{!isLoaded && <></>}
-
-{isLoaded && <GoogleMap
-    mapContainerStyle={containerStyle}
-    center={center}
-    zoom={3}
-    onLoad={onLoad}
-    onUnmount={onUnmount}
-    options={{
-      minZoom: 3,
-      streetView: null,
-      disableDefaultUI: true,
-    }}
->
-  <slot />
-  <Marker position={center}/>
-</GoogleMap>
-}
-
-<script>
-
-</script>
+export default memo(GoogleMaps);
